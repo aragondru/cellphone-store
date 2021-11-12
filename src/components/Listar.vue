6f97863129ecef61e228193718996745d83dc651
Alejandro Aragon <ar17092@ues.edu.sv>
2021-11-11 23:32:42 -0600

    Conexion a firebase y obteniendo datos

---
+++ src/components/Listar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h2>Hola desde listar</h2>
+        <ul>
+            <li v-for="(cell,key) in cellphone" :key="key">{{cell.marca}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import {db} from '../db'
+    export default {
+        name: 'Listar',
+        data() {
+            return {
+                cellphone:[],
+            }
+        },
+        firestore: {
+            cellphone: db.collection('cellphone'),
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,15 @@
 <template>
-  <hello-world />
+  <listar />
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+  import Listar from '../components/Listar.vue'
 
   export default {
     name: 'Home',
 
     components: {
-      HelloWorld,
+      Listar,
     },
   }
 </script>
